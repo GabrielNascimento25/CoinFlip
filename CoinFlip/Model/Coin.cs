96264342e8fc43a333f8598f6ca9ae473f86ac4a
etechas <etechas@LI02-16>
2024-06-19 17:58:26 -0300

    Add project files.

---
+++ CoinFlip/Model/Coin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace CoinFlip.Model
+{
+    internal class Coin
+    {
+        private Random random;
+        public Coin()
+        {
+            random = new Random();
+        }
+
+        public object Lado { get; internal set; }
+
+        public string Jogar(string ladoEscolhido)
+        {
+            int resultado = random.Next(2);
+            return resultado == 0 ? "Cara" : "Coroa";
+        }
+    }
+}
+
